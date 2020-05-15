4f31bf08f26e6c5b4c20de6be7cc4098b73820d5
ishikaahuja <57452619+ishikaahuja@users.noreply.github.com>
2020-05-15 15:55:13 +0530

    Add files via upload
    assignment1

---
+++ clas1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ISHIKA
+ */
+public class clas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        // TODO code application logic here
+    }
+    
+}
